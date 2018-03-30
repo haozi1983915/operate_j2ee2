5ded198954b6aa9d6c50f60e98fdc8e880db2160
astro <zhi.nie@maimob.cn>
2018-03-30 15:05:13 +0800

    findBalanceAccountById  方法

--- KHW/src/com/maimob/server/db/daoImpl/BalanceAccountDaoImpl.java
+++ KHW/src/com/maimob/server/db/daoImpl/BalanceAccountDaoImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
+import com.alibaba.fastjson.JSONObject;
 import com.maimob.server.db.common.BaseDaoHibernate5;
 import com.maimob.server.db.entity.BalanceAccount; 
  
@@ -20,6 +21,20 @@ public class BalanceAccountDaoImpl extends BaseDaoHibernate5<BalanceAccount>{
     	try {
     		BalanceAccounts = sessionFactory.getCurrentSession()
             .createQuery("select en from BalanceAccount en "+where) 
+            .getResultList();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        return BalanceAccounts;
+    }
+    
+    @SuppressWarnings("unchecked") 
+    public List<BalanceAccount> findBalanceAccountById(JSONObject jobj) {
+    	List<BalanceAccount> BalanceAccounts = new ArrayList<BalanceAccount>();
+    	int id = jobj.getIntValue("ourCompanyId");
+    	try {
+    		BalanceAccounts = sessionFactory.getCurrentSession()
+            .createQuery("select en from BalanceAccount en where en.id = " + id) 
             .getResultList();
 		} catch (Exception e) {
 			// TODO: handle exception
