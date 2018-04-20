b436608897decf4e748ea203f299afc3d0543301
yangfeng <feng.yang@maimob.cn>
2018-04-20 09:44:49 +0800

    新增埋点数据service

--- KHW/src/com/maimob/server/service/MicroService.java
+++ KHW/src/com/maimob/server/service/MicroService.java
@@ -1,6 +1,9 @@
 package com.maimob.server.service;
 
+import com.maimob.server.base.BasicPage;
 import com.maimob.server.base.BasicRequest;
+import com.maimob.server.db.entity.OperateActionIdcard;
+import com.maimob.server.db.entity.OperateActionSubmit;
 import com.maimob.server.db.entity.OperateReportFormAppToday;
 
 import java.util.List;
@@ -10,5 +13,9 @@ import java.util.List;
  */
 public interface MicroService {
 
-    List<OperateReportFormAppToday> summary(BasicRequest request);
+    List<OperateReportFormAppToday> summary(BasicPage<OperateReportFormAppToday> page, BasicRequest request);
+
+    List<OperateActionIdcard> applyRetention(BasicPage<OperateActionIdcard> page, BasicRequest request);
+
+    List<OperateActionSubmit> openRetention(BasicPage<OperateActionSubmit> page, BasicRequest request);
 }
--- KHW/src/com/maimob/server/service/impl/MicroServiceImpl.java
+++ KHW/src/com/maimob/server/service/impl/MicroServiceImpl.java
@@ -1,12 +1,17 @@
 package com.maimob.server.service.impl;
 
+import com.maimob.server.base.BasicPage;
 import com.maimob.server.base.BasicRequest;
+import com.maimob.server.db.daoImpl.OperateActionIdcardDaoImpl;
+import com.maimob.server.db.daoImpl.OperateActionSubmitImpl;
 import com.maimob.server.db.daoImpl.OperateReportFormAppTodayImpl;
+import com.maimob.server.db.entity.OperateActionIdcard;
+import com.maimob.server.db.entity.OperateActionSubmit;
 import com.maimob.server.db.entity.OperateReportFormAppToday;
 import com.maimob.server.service.MicroService;
+import com.wordnik.swagger.annotations.ApiModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,8 +24,24 @@ public class MicroServiceImpl implements MicroService {
     @Autowired
     private OperateReportFormAppTodayImpl dao;
 
+    @Autowired
+    private OperateActionIdcardDaoImpl operateActionIdcardDao;
+
+    @Autowired
+    private OperateActionSubmitImpl actionSubmitDao;
+
     @Override
-    public List<OperateReportFormAppToday> summary(BasicRequest request) {
-        return dao.summary(request);
+    public List<OperateReportFormAppToday> summary(BasicPage<OperateReportFormAppToday> page, BasicRequest request) {
+        return dao.summary(page, request);
+    }
+
+    @Override
+    public List<OperateActionIdcard> applyRetention(BasicPage<OperateActionIdcard> page, BasicRequest request) {
+        return operateActionIdcardDao.applyRetention(page, request);
+    }
+
+    @Override
+    public List<OperateActionSubmit> openRetention(BasicPage<OperateActionSubmit> page, BasicRequest request) {
+        return actionSubmitDao.applyRetention(page, request);
     }
 }
